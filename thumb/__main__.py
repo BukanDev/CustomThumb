2fa53f17830caffbc080d064ad0ccce2111ec0d6
ajual7832@gmail.com <ajual7832@gmail.com>
2023-04-09 04:27:20 +0700

    0

--- thumb/__main__.py
+++ thumb/__main__.py
@@ -11,12 +11,25 @@ from thumb import bot
 from asyncio import get_event_loop_policy
 from pyrogram.methods.utilities.idle import idle
 from thumb.plugins import ALL_MODULES
+from pyrogram.types import BotCommand, BotCommandScopeAllPrivateChats
+
 
 async def main():
     await bot.start()
     for all_module in ALL_MODULES:
         importlib.import_module(f"thumb.plugins.{all_module}")
     print(f"{bot.me.first_name} | @{bot.me.username}Telah aktif")
+    await bot.set_bot_commands(
+            [
+                BotCommand("start", "start bot"),
+                BotCommand("help", "helppers bot"),
+                BotCommand("thumb", "change tumb video"),
+                BotCommand("rthumb", "remove thumb video"),
+                BotCommand("cekthumb", "cek custom thumb"),
+                BotCommand("rename", "reply to video to modif thumb"),
+            ],
+            BotCommandScopeAllPrivateChats(),
+        )
     await idle()
 
 if __name__ == "__main__" :
